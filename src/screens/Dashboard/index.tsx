b3894aa124b96f62532d45e9e67f0b61749b5026
Victor Codonho <victorenrico50@gmail.com>
2021-07-13 20:55:45 -0300

    layout

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { HighLightCard } from '../../components/HighLightCard';
 
 import {
-    Container
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
 } from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Text>Dashboard</Text>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/26679579?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Victor</UserName>
+                        </User>
+                    </UserInfo>
+
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+
+            <HighLightCard />
         </Container>
     );
 }
